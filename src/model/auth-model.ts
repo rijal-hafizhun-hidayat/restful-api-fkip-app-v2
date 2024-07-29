922e2290ea40b1ad8d07fbf81bd1f020ca90a5fc
unknown <rijal.1344@gmail.com>
2024-07-29 21:27:59 +0700

    feat: api profile

--- src/model/auth-model.ts
+++ src/model/auth-model.ts
@@ -1,22 +1,40 @@
-import { user } from "@prisma/client"
+import { user } from "@prisma/client";
 
 export type LoginRequest = {
-    username: string,
+  username: string;
+  password: string;
+};
+
+export type TokenRequest = {
+  token: string;
+};
+
+export type UpdatePasswordRequest = {
     password: string
 }
 
-export type TokenRequest = {
-    token: string
-}
-
 export type LoginResponse = {
-    name: string,
-    token?: string | null
+  name: string;
+  token?: string | null;
+};
+
+export type UpdateProfileRequest = {
+  name: string;
+  username: string;
+  email: string;
+};
+
+export function toLoginResponse(user: user, token: string): LoginResponse {
+  return {
+    name: user.name,
+    token: token,
+  };
 }
 
-export function toLoginResponse(user: user, token: string): LoginResponse{
-    return {
-        name: user.name,
-        token: token
-    }
+export function toUpdateProfileRequest(user: user): UpdateProfileRequest {
+  return {
+    name: user.name,
+    username: user.username,
+    email: user.email,
+  };
 }
