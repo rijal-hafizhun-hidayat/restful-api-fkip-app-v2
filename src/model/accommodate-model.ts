ee49970867034216ad6f3200dc11f1a3f2e39c7d
unknown <rijal.1344@gmail.com>
2024-08-07 18:39:19 +0700

    feat: add accommodate colleger

--- src/model/accommodate-model.ts
+++ src/model/accommodate-model.ts
@@ -1,4 +1,20 @@
+import { accommodates } from "@prisma/client";
+
 export type AccommodateRequest = {
   user_id?: number,
   user_id_accommodate: number
 };
+
+export type AccommodateResponse = {
+  user_id_dpl: number | null,
+  user_id_tutor_teacher: number | null,
+  user_id_colleger: number | null
+}
+
+export function toAccommodateResponse(accommodate: accommodates): AccommodateResponse{
+  return {
+    user_id_dpl: accommodate.user_id_dpl,
+    user_id_tutor_teacher: accommodate.user_id_tutor_teacher,
+    user_id_colleger: accommodate.user_id_colleger
+  }
+}
